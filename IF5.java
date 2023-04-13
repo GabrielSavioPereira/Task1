773df82b2d6b32da8c6ba4d5ad0331b64e4832d4
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-04-13 08:02:09 -0300

    Update IF5.java

--- IF5.java
+++ IF5.java
@@ -7,18 +7,21 @@ public class IF5{
         Scanner sc = new Scanner(System.in);
 
         int num = Integer.MIN_VALUE;
+        // while para continuar pedindo 
         while (true) {
             System.out.println("Digite um número:");
-            while (true) {
+            while (true) { // enquanto não digitar um número válido, continua no while
                 try {
                     num = Integer.parseInt(sc.nextLine());
                     System.out.println("O número digitado é: "+ num);
-                    break; 
+                    break; // se chegou aqui é porque o número é válido, então posso sair do while
                 } catch (NumberFormatException e) {
+                    // número inválido
                     System.out.println("Digite um número válido");
                 }
             }
         
+            // se for -1, sai do while mais externo
             if (num != Integer.MIN_VALUE){
                 break;
 
